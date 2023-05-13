de8af1d387f703ef34ecc3a85e2a677f7a7b26fc
yeasinbinali <yeasinbinali@gmail.com>
2023-05-13 06:56:46 +0600

    add new service img setup

--- src/pages/Dashboard/AddService/AddService.js
+++ src/pages/Dashboard/AddService/AddService.js
@@ -30,8 +30,24 @@ const AddService = () => {
                 price: data.price,
                 time: data.time,
                 barber: data.barber,
+                slots: [
+                  '8:00am',
+                  '9:00am',
+                  '10:00am',
+                  '11:00am',
+                  '12:00am',
+                  '1:00pm',
+                  '2:00pm',
+                  '3:00pm',
+                  '4:00pm',
+                  '5:00pm',
+                  '6:00pm',
+                  '7:00pm',
+                  '8:00pm',
+                ],
                 img: imgData.data.url
             }
+            console.log(serviceDetail);
             fetch('http://localhost:5000/services', {
                 method: 'POST',
                 headers: {
